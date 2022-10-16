7769ce622d16f186db3b46c232082dffb8040f47
Roberto <47633569+PttsDev@users.noreply.github.com>
2022-10-16 18:55:53 +0200

    Cmake y tests pasados

--- exampleFunction.cpp
+++ exampleFunction.cpp
@@ -3,18 +3,44 @@
 #include <stdio.h>
 #include <stdint.h> 
 
+/* Fix by checking if the addition will overflow the UINT type */
 unsigned int wrapFunctionAdd(unsigned int ui_a, unsigned int ui_b) {
-  unsigned int usum = ui_a + ui_b;
+  unsigned int usum;
+
+  if(UINT_MAX - ui_a < ui_b) {
+    usum = UINT_MAX;
+  } else {
+    usum = ui_a + ui_b;
+  }
+
   return usum;
 }
 
+/* Fix by checking if the mult will overflow the UINT type */
 unsigned int wrapFunctionMul(unsigned int ui_a, unsigned int ui_b) {
+
   unsigned umul = ui_a;
-  umul *= ui_b;
+
+  if(ui_a == 0) {
+    umul = 0;
+  } else if(ui_b > UINT_MAX / ui_a) {
+    umul = UINT_MAX;
+  } else {
+    umul *= ui_b;
+  }
+
   return umul;
 }
 
+/* Fix by checking if the expression is not shifted by a negative number of bits or by greater than or equal to the number of bits that exist in the operand */
 uint32_t wrapFunctionShift(uint32_t ui_a, unsigned int ui_b) {
-  uint32_t uShift = ui_a << ui_b | ui_a >> (32 - ui_b); 
+  uint32_t uShift = 0;
+
+  if(ui_b >= 32) {
+    uShift = 0;
+  } else {
+    uShift = ui_a << ui_b | ui_a >> (32 - ui_b);
+  }
+
   return uShift;
 }
